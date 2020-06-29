0f2f8726bf86d2ccf1d95517d4b3d8f77fe261a5
FareedaSaleh <fr.alharbi@hotmail.com>
2020-06-29 20:51:30 +0300

    add

--- app.py
+++ app.py
@@ -259,5 +259,8 @@ def create_app(test_config=None):
 
 APP = create_app()
 
-if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+# if __name__ == '__main__':
+#     APP.run(host='0.0.0.0', port=8080, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
