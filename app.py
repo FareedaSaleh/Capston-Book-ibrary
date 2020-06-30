e8050f633c57a76d364d57c7f3e909bf0042f479
Fareeda Saleh <fr.alharbi.com>
2020-06-30 13:04:41 +0300

    initial commit

--- app.py
+++ app.py
@@ -265,4 +265,4 @@ APP = create_app()
 if __name__ == '__main__':
     # APP.run(host='0.0.0.0', port=8080, debug=True)
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    APP.run(host='0.0.0.0', port=port, debug=True)
