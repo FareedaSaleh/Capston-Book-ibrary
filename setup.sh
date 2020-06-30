4918084f1db0973f48d92620c90120d677dd48b4
Fareeda Saleh <fr.alharbi.com>
2020-06-30 12:48:50 +0300

    initial commit

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ export API_AUDIENCE ='http://localhost:5000'
 export CLINT_ID='9UXpyB2qhXB48l6frCDhPofrSZjmyhfA'
 export FLASK_APP=app.py
 export DATABASE_URL='postgres://ayrojxosrdycfl:1879923d9fad9601ba959eea32940b768854f1f66a7b02a77818ab2678083dce@ec2-34-192-173-173.compute-1.amazonaws.com:5432/dfr9u71jrqqbdr'
+export FLASK_ENV=development
