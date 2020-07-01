746a45adb1c3727ff75429b851b99722c5e76fca
Fareeda Saleh <fr.alharbi.com>
2020-07-01 19:12:03 +0300

    fix:os env

--- auth/auth.py
+++ auth/auth.py
@@ -3,11 +3,15 @@ from flask import request, _request_ctx_stack, abort
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
+import os
 
 
-AUTH0_DOMAIN = 'publiclibrary.us.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'http://localhost:5000'
+AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
+ALGORITHMS = os.environ.get('ALGORITHMS')
+API_AUDIENCE = os.environ.get('API_AUDIENCE')
+# AUTH0_DOMAIN = 'publiclibrary.us.auth0.com'
+# ALGORITHMS = ['RS256']
+# API_AUDIENCE = 'http://localhost:5000'
 
 # AuthError Exception
 '''
